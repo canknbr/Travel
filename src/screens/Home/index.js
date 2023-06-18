e4965c9dbdbf2fac9c4d8c77693e4452b83944d3
canknbr <cankanbur1@gmail.com>
2023-06-18 20:39:22 +0300

    init

---
+++ src/screens/Home/index.js
@@ -0,0 +1,14 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import Title from '../../components/Title';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Title />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
